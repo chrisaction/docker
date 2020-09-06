0c37035d858cc0add7d549488390decbdcc27239
小朋 <2810580330@qq.com>
2020-09-06 13:52:34 +0800

    archlinux

--- ibm-docker/entrypoint.sh
+++ ibm-docker/entrypoint.sh
@@ -1,36 +1,4 @@
 #!/bin/sh
-#ssh
-cat <<-EOF > /usr/local/etc/v2ray/config.json
-{
-  "inbounds": [
-  {
-    "port": 8080,
-    "listen":"127.0.0.1",
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
-        {
-          "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
-          "alterId": 4       
-        }
-      ]
-    },
-    "streamSettings": {
-      "network": "ws",
-      "wsSettings": {
-      "path": "/ws"
-      }     
-    }
-  }
-  ],
-  "outbounds": [
-  {
-    "protocol": "freedom",
-    "settings": {}
-  }
-  ]
-}
-EOF
-nohup caddy -conf="/etc/Caddyfile"   >/usr/local/etc/caddy.txt 2>&1 &
+nohup caddy -conf="/etc/Caddyfile"   >/etc/caddy.txt 2>&1 &
 # Run V2Ray
-v2ray -config /usr/local/etc/v2ray/config.json
+v2ray -config https://raw.githubusercontent.com/byxiaopeng/ads/master/vmess.json
