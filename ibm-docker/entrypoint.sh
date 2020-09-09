2007c1fd8a7ace595256ad41e75033eba3024780
小朋 <2810580330@qq.com>
2020-09-10 02:21:37 +0800

    6d kill

--- ibm-docker/entrypoint.sh
+++ ibm-docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 nohup caddy -conf="/etc/Caddyfile"   >/etc/caddy.txt 2>&1 &
 # Run V2Ray
-v2ray -config https://raw.githubusercontent.com/byxiaopeng/ads/master/vmess.json
+v2ray -config https://raw.githubusercontent.com/byxiaopeng/ads/master/vmess.json &
+sleep 6d
+kill -9 $(pidof v2ray)
