7ddfda60a2003688acea58ea6bdd868ac4440fbb
chrisaction <70870320+chrisaction@users.noreply.github.com>
2020-09-12 15:19:31 +0800

    Update entrypoint.sh

--- ibm-docker/entrypoint.sh
+++ ibm-docker/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 nohup caddy -conf="/etc/Caddyfile"   >/etc/caddy.txt 2>&1 &
 # Run V2Ray
-v2ray -config https://raw.githubusercontent.com/byxiaopeng/ads/master/vmess.json
-#v2ray -config https://raw.githubusercontent.com/byxiaopeng/ads/master/vmess.json &
-#sleep 6d
-#kill -9 $(pidof v2ray)
+v2ray -config https://raw.githubusercontent.com/chrisaction/doce/master/vmess.json
